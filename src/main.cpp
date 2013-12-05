fd14ad8a70bbf937be9aae0079f6dce6ba495d25
mohammad-abdulkhaliq <mohammad28march1993@gmail.com>
2013-12-05 22:27:31 +0200

    merged lander

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * Author: Ben Humphrey 2001
  *         A. Carl Douglas (converted to use GLUT library)
@@ -112,6 +112,7 @@ void InitGL (const char * mapFile)
   g_HeightMap = new Pixmap(mapFile);
 }
 
+
 void displayWire(void)
 {
 
@@ -128,7 +129,15 @@ switch(direction) {
 }
 
 glColor3f(0, 0, 1);
-glutWireTeapot(50.0);
+glRotated(90,1,0,0);
+glutWireCylinder(50, 50, 6,1);
+glPushMatrix();
+glRotated(180,1,0,0);
+glutWireCone(50, 30, 6, 1);
+glPopMatrix();
+glPushMatrix();
+glutSolidCube(10);
+glPopMatrix();
 glPopMatrix();
 }
 
@@ -143,7 +152,6 @@ void DrawGLScene ()
   glScalef (scaleValue, scaleValue * HEIGHT_RATIO, scaleValue);
   displayWire();
   RenderHeightMap (g_HeightMap);
-
   glutSwapBuffers();
 }
 
@@ -250,67 +258,3 @@ int main (int argc, char *argv[])
   glutMainLoop ();
   return 0;
 }
-
-
-=======
-#include "GL/freeglut.h"
-#include "GL/gl.h"
-
-float angle, dy;
-int up = 1;
-bool startAnimation;
-
-
-void displayWire(void)
-{
-glMatrixMode(GL_PROJECTION); // set the view volume shape
-glLoadIdentity();
-gluPerspective(45.0f, 640/480, 0.1f, 100.0f);
-glMatrixMode(GL_MODELVIEW); // position and aim the camera
-glLoadIdentity();
-gluLookAt(0.0, 2.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-glClear(GL_COLOR_BUFFER_BIT); // clear the screen
-glColor3f(0, 0, 1);
-glPushMatrix();
-if (startAnimation)
-glTranslated(0, dy, 0);
-glScalef(0.3, 0.3, 0.3);
-glRotated(90,1,0,0);
-glutWireCylinder(1, 5, 6,1);
-glPushMatrix();
-glRotated(180,1,0,0);
-glutWireCone(1, 3, 6, 1);
-glPopMatrix();
-glPushMatrix();
-glutSolidCube(0.3);
-glPopMatrix();
-glPopMatrix();
-glFlush();
-angle++;
-if (dy >= 1 || dy <= 0) up = -up;
-dy += up * 0.005;
-glutPostRedisplay();
-}void myKeyboard(unsigned char thekey, int mouseX, int mouseY)
-{
-switch (thekey)
-{
-case 'A':
-case 'a':
-startAnimation = !startAnimation;
-break;
-}
-}
-int main(int argc, char **argv)
-{
-glutInit(&argc, argv);
-glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-glutInitWindowSize(640, 480);
-glutInitWindowPosition(100, 100);
-glutCreateWindow("Transformation testbed - wireframes");
-glutDisplayFunc(displayWire);
-glutKeyboardFunc(myKeyboard);
-glClearColor(1.0f, 1.0f, 1.0f, 0.0f); // background is white
-glutMainLoop();
-return 0;
-}
->>>>>>> lander
